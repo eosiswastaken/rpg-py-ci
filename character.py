56582aadf833f786ecf86990c169ee29fdd24d19
TheLordGibril <gabidule03@gmail.com>
2024-12-19 11:41:01 +0100

    zeogegz

--- character.py
+++ character.py
@@ -4,3 +4,8 @@ class Character:
 
     def get_hp(self):
         return self.__hp
+
+    def recieve_damage(self):
+        self.__hp -= 1
+        if self.__hp < 0:
+            self.__hp = 0
--- tests_rpg.py
+++ tests_rpg.py
@@ -8,6 +8,17 @@ class TestRpg(unittest.TestCase):
         bobasis = character.Character()
         self.assertEqual(10, bobasis.get_hp())
 
+    def test_ten_attacks_kill(self):
+        bobasis = character.Character()
+        for i in range(10):
+            bobasis.recieve_damage()
+        self.assertEqual(0, bobasis.get_hp())
+
+    def test_nine_attacks_dont_kill(self):
+        bobasis = character.Character()
+        for i in range(9):
+            self.assertLess(0, bobasis.get_hp())
+
 
 if __name__ == '__main__':
     unittest.main()
